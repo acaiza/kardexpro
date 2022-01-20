4a30aaa2e9142b738f15e2841cd45be6f6ed9fff
Andres Caiza <www.mundoandres@gmail.com>
2022-01-19 20:13:02 -0500

    Estructurar del proyecto

---
+++ src/main/java/ec/com/ibs/kardexpro/KardexproApplication.java
@@ -0,0 +1,13 @@
+package ec.com.ibs.kardexpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardexproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/ibs/kardexpro/KardexproApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.ibs.kardexpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardexproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
